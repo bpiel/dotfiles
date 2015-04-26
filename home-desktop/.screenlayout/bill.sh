0f2bea27f647629c1b8f55c419c131dda2ec6348
Bill Piel <bill@billpiel.com>
2015-04-26 10:20:27 -0400

    add background and screen setup

---
+++ home-desktop/.screenlayout/bill.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DisplayPort-0 --off --output DVI-1 --mode 1280x1024 --pos 0x0 --rotate normal --output DVI-0 --mode 1680x1050 --pos 1280x0 --rotate normal --output HDMI-0 --off
