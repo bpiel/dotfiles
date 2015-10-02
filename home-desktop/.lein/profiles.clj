cbc23f53ffcfd01ef0ad3dcf6d737021f24ed4c6
Bill Piel <bill@billpiel.com>
2015-10-01 23:12:39 -0400

    add to profiles.clj [org.clojure/tools.trace 0.7.7-BILL-SNAPSHOT]

--- home-desktop/.lein/profiles.clj
+++ home-desktop/.lein/profiles.clj
@@ -1,5 +1,6 @@
 {:user {:plugins [[cider/cider-nrepl "0.9.1"]
                   [refactor-nrepl "1.0.5"]]
         :dependencies [[spyscope "0.1.5"]
-                       [org.clojure/tools.nrepl "0.2.9"]]
+                       [org.clojure/tools.nrepl "0.2.9"]
+                       [org.clojure/tools.trace "0.7.7-BILL-SNAPSHOT"]]
         :injections [(require 'spyscope.core)]}}
