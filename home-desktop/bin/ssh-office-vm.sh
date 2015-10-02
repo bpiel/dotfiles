6325d580d0ce0ee7af1727a04cf5eb186391ecb7
Bill Piel <bill@billpiel.com>
2015-10-01 23:13:51 -0400

    office vpn ssh scripts

---
+++ home-desktop/bin/ssh-office-vm.sh
@@ -0,0 +1,1 @@
+ssh -i /home/bill/.ssh/private_key.office-vm vagrant@bill.office.rjmetrics.com
---
+++ home-desktop/bin/sshfs-office-vm.sh
@@ -0,0 +1,1 @@
+sshfs /home/bill/vm/ vagrant@bill.office.rjmetrics.com:/opt/code  -o IdentityFile=/home/bill/.ssh/private_key.office-vm
