cefc7cf5af8efc9c3c117e05f511da698691d659
Bill Piel <bpiel@rjmetrics.com>
2016-03-18 02:09:39 -0400

    mount vm dir to /opt/code

--- home-desktop/bin/refresh-emacs-locatedb.sh
+++ home-desktop/bin/refresh-emacs-locatedb.sh
@@ -1,2 +1,2 @@
 updatedb -l 0 -U /home/bill/repos -o /home/bill/.emacs.d/locatedbs/repos.locatedb
-updatedb -l 0 -U /home/bill/vm/ -o /home/bill/.emacs.d/locatedbs/vm.locatedb
+updatedb -l 0 -U /opt/code -o /home/bill/.emacs.d/locatedbs/vm.locatedb
