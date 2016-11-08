9af1eb314c14e4c06bd655131c19a07fa1d4b33e
Bill Piel <bill@billpiel.com>
2016-11-07 20:34:54 -0500

    update home-desktop

--- home-desktop/bin/sshfs-office-vm.sh
+++ home-desktop/bin/sshfs-office-vm.sh
@@ -1,1 +1,2 @@
-sshfs /home/bill/vm/ vagrant@bill.office.rjmetrics.com:/opt/code  -o IdentityFile=/home/bill/.ssh/private_key.office-vm
+# sshfs /opt/code vagrant@bill.office.rjmetrics.com:/opt/code  -o IdentityFile=/home/bill/.ssh/private_key.office-vm
+sshfs office-vm:/opt/code /opt/code
---
+++ home-desktop/bin/sshfs-vm.sh
@@ -0,0 +1,1 @@
+sudo sshfs -o allow_other -o directport=7777 10.10.10.4:/opt/code /opt/code
