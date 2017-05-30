58303bb28f34a5aa357ca09257b8b73b96e37f72
Bill Piel <bpiel@rjmetrics.com>
2017-05-30 15:24:00 -0400

    work -- border blue; lock screen

--- work-desktop/.xmonad/xmonad.hs
+++ work-desktop/.xmonad/xmonad.hs
@@ -80,7 +80,7 @@ myWorkspaces    = ["1","2","3","4","5","6","7","8","9"]
 -- Border colors for unfocused and focused windows, respectively.
 --
 myNormalBorderColor  = "#dddddd"
-myFocusedBorderColor = "#ff0000"
+myFocusedBorderColor = "#386890"
 
 ------------------------------------------------------------------------
 -- Key bindings. Add, modify or remove key bindings here.
@@ -95,6 +95,9 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
 
     -- launch gmrun
     --, ((modm .|. shiftMask, xK_p     ), spawn "gmrun")
+
+    -- lock screen
+    , ((modm .|. controlMask .|. mod1Mask, xK_l     ), spawn "/home/bill/bin/lock.sh")
 
     -- close focused window
     , ((modm .|. shiftMask, xK_c     ), kill)
