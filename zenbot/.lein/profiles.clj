7e16105b26fc8b5a81e4eef3c56e167c408f192d
Bill Piel <bill@billpiel.com>
2018-04-06 17:07:22 -0400

    zenbot: ubuntu 16.04.03

--- zenbot/.lein/profiles.clj
+++ zenbot/.lein/profiles.clj
@@ -1,10 +1,3 @@
-{:user {:plugins [[cider/cider-nrepl "0.9.1"]
-                  [refactor-nrepl "1.0.5"]]
-        :dependencies [[spyscope "0.1.5"]
-                       [org.clojure/tools.nrepl "0.2.9"]
-                       [com.billpiel/sayid "0.0.2-dev"]]
-        :injections [(require 'spyscope.core)]
-        :repl-options {:nrepl-middleware [com.billpiel.sayid.nrepl-middleware/wrap-sayid]
-                       :init (do (ns -)
-                                 (def p clojure.pprint/pprint))
-                       :timeout 120000}}}
+{:user {:plugins [[cider/cider-nrepl "0.15.1"]
+                  [com.billpiel/sayid "0.0.16"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
